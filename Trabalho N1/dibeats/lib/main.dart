9146be61eda45dde6fd64b6e78651201d3bf1a2e
sirotaiv <otaviorau@gmail.com>
2023-12-06 22:55:26 -0300

    Implementado login e cadastro de usuários

--- Trabalho N1/dibeats/lib/main.dart
+++ Trabalho N1/dibeats/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dibeats/requests/request.dart';
 import 'package:dibeats/system_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,6 +11,12 @@ Future main() async {
   runApp(const MyApp());
 }
 
+class ErrorStatus {
+  String code;
+  String message;
+
+  ErrorStatus({required this.code, required this.message});
+}
 class MyApp extends StatelessWidget {
 
   const MyApp({super.key});
@@ -55,6 +62,56 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final TextEditingController _loginInput = TextEditingController();
+  final TextEditingController _passwordInput = TextEditingController();
+
+  Future<void> _normalLogin() async {
+    String email = _loginInput.text.trim();
+    String password = _passwordInput.text;
+
+    try{
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email, 
+        password: password
+      );
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const SystemPage())
+      );
+    } on FirebaseAuthException catch(e) {
+
+      ErrorStatus errorStatus;
+      
+      if(e.code == 'user-not-found') {
+        errorStatus = ErrorStatus(code: e.code, message: 'User not found or doesnt exist');
+      } else if(e.code == 'wrong-password') {
+        errorStatus = ErrorStatus(code: e.code, message: 'Wrong password, plese verify');
+      } else {
+        errorStatus = ErrorStatus(code: e.code, message: e.message!);
+      }
+
+      showDialog(context: context, 
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Failed to login'),
+          content: Text('Code: ${errorStatus.code}\nMessage: ${errorStatus.message}'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Fechar'),
+            ),
+          ],
+        );
+      });
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,9 +136,33 @@ class _MyHomePageState extends State<MyHomePage> {
               Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  const TextField(decoration: InputDecoration(filled: true, fillColor: Colors.white, labelText: "Username", border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))), ),),
+                  TextField(
+                    controller: _loginInput,
+                    decoration:const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: "Username", 
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20)
+                          )
+                        ),
+                       ),
+                      ),
                   const SizedBox(height: 15.0,),
-                  const TextField(decoration: InputDecoration(filled: true, fillColor: Colors.white, labelText: "Password", border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),),),
+                  TextField(
+                    controller: _passwordInput,
+                    decoration:const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: "Password", 
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20)
+                          )
+                        ),
+                      ),
+                    ),
                   const SizedBox(height: 15.0,),
                   SizedBox(
                     width: 400,
@@ -90,11 +171,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          FloatingActionButton.extended(onPressed: () => setState(() {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(builder: (context) => const SystemPage())
-                            );
-                          }),
+                          FloatingActionButton.extended(onPressed: () async => _normalLogin(),
                           backgroundColor: Colors.black,
                           label: const Text('Login', style: TextStyle(fontSize: 20, color: Colors.green),)),
                         ],
